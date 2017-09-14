6189d69e9f7321f21e226b0e143f2f814bdef7e4
Herculys <Herculys@DESKTOP-8R2J0VT.home>
2017-09-14 02:24:55 -0300

    Update

--- Lab1/src/Teste.java
+++ Lab1/src/Teste.java
@@ -1,9 +1,22 @@
+import java.util.Scanner;
 
 public class Teste {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
-
+		
+		Scanner sc = new Scanner(System.in);
+		int n1, n2, n3;
+		
+		System.out.println("Informe o primeiro numero:");
+		n1 = sc.nextInt();
+		
+		System.out.println("Informe o segundo numero:");
+		n2 = sc.nextInt();
+		
+		System.out.println("Informe o terceiro numero:");
+		n3 = sc.nextInt();
+		
+		
 	}
 
 }
