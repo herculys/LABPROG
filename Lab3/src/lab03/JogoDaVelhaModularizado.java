ecb2b069ae712790414a371b81bb9f8dc12bfe70
Herculys <herculyys@gmail.com>
2017-09-25 04:08:44 -0300

    Backup base

---
+++ Lab3/src/lab03/JogoDaVelhaModularizado.java
@@ -0,0 +1,14 @@
+package lab03;
+
+/*
+ * Aluno: Herculys
+ */
+
+import java.util.Scanner;
+
+public class JogoDaVelhaModularizado {
+	public static void main(String args[]){
+		Metodos methods = new Metodos();
+		methods.iniciarOJogo();	
+	}
+}
